7e38ecf5ec5fa789fb0e8efd683009bdeea64dce
GhostPicker <90055659+GhostPicker@users.noreply.github.com>
2021-12-29 23:05:36 +0300

    Контрольная работа

---
+++ KR/KR/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR
+{
+    public sealed  class Button
+    {
+        public static string Color;
+        public int Height { get; set; }
+        public int Width { get; set; }
+       
+        public Button(string color = "", int height = 0, int width = 0)
+        {
+
+            Color = color;
+            Height = height;
+            Width = width;
+
+        }
+        public Button(Button a)
+        {
+
+            
+            Height = a.Height;
+            Width = a.Width;
+
+        }
+       
+
+       
+          void Squere(int b,int c)
+        {
+           int s = b * c;
+            Console.WriteLine("Squere=" + s);
+        } 
+        
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Button b = obj as Button;
+            if (b == null)
+                return false;
+            return  this.Width == b.Width && this.Height == b.Height;
+        }
+        interface IEvent {
+            void Click();
+        }
+        interface IAction { void Click(); }
+    }
+}
---
+++ KR/KR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //Вариант 17
+           //1 задание
+           //a
+            Console.WriteLine("Max ulong="+ulong.MaxValue);
+            //б
+            string a = "gfdsfgOhfiuhO";
+            int k = a.IndexOf('O');
+            Console.WriteLine("Index first O="+k);
+            Console.WriteLine("\n");
+            //2 задание
+            Button b = new Button("Красный",20,12);
+            Button c = new Button("Желтый", 13, 21);
+            if (b.Equals(c))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+             
+            Squere(b.Height,b.Width);
+          
+        }
+    }
+}
